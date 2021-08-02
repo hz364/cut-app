525bda945b0b00368bf697c0702b3ef67f88710e
hz364 <75688223+hz364@users.noreply.github.com>
2021-08-02 16:54:14 +0100

    Basic functionality enabled

---
+++ server.py
@@ -0,0 +1,26 @@
+import numpy as np
+from bokeh.core.property.primitive import Null
+import csv
+import os
+from bokeh.io import save
+from bokeh.models import Slider
+from bokeh.util.browser import view
+from bokeh.io import curdoc
+from bokeh.plotting import figure, ColumnDataSource
+from bokeh.layouts import row, column
+from bokeh.models import Div, ColumnDataSource, CustomJS, FileInput
+from bokeh.plotting import figure, save
+from bokeh.layouts import column
+from bokeh.models import CustomJS, ColumnDataSource, Slider, ranges
+from bokeh.plotting import Figure, output_file, show
+from bokeh.models import Button  # for saving data
+from bokeh.events import ButtonClick  # for saving data
+from bokeh.models.widgets import DataTable, DateFormatter, TableColumn
+from bokeh.models import HoverTool
+from bokeh.io import show
+from bokeh.models import ColumnDataSource, RangeTool
+from bokeh.plotting import figure,curdoc
+
+
+
+
