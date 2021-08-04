318b759a7114ba8fa5c5742b2a2c83e8dd247842
hz364 <75688223+hz364@users.noreply.github.com>
2021-08-04 14:38:36 +0100

    Added parameters saving functionality , changed compared to 1 for divided
    changed button for load data

--- main.py
+++ main.py
@@ -18,10 +18,95 @@ global s1
 global s3
 global s4
 global s5
-
+global s6
 # Load data from csv file
 file_input_label = Div(text='Please select a file:')
-file_input = FileInput()
+file_input = FileInput(css_classes=["my_fileInput"])
+style = Div(text="""
+<style>
+::-webkit-file-upload-button {
+  background: #e2e2e2;
+  color: black;
+  position: relative;
+  height: 40px;
+  width: 110px;
+  margin: 0 10px 18px 0;
+  text-decoration: none;
+  font: 12px "Helvetica Neue", Helvetica, Arial, sans-serif;
+  font-weight: bold;
+  line-height: 25px;
+  text-align: center; 
+
+  -webkit-border-radius: 4px; 
+  -moz-border-radius: 4px;
+  border-radius: 4px;
+}
+::-webkit-file-upload-button:hover {
+    background: #e2e2e2;
+    background: -webkit-gradient(linear, left top, left bottom, from(#e2e2e2), to(#eee));
+    background: -moz-linear-gradient(top,  #e2e2e2,  #eee);
+}
+
+::-webkit-file-upload-button:visited {
+    color: #555;
+    border-bottom: 4px solid #b2b1b1;
+    text-shadow: 0px 1px 0px #fafafa;
+     
+    background: #eee;
+    background: -webkit-gradient(linear, left top, left bottom, from(#eee), to(#e2e2e2));
+    background: -moz-linear-gradient(top,  #eee,  #e2e2e2);
+     
+    box-shadow: inset 1px 1px 0 #f5f5f5;
+}
+
+::-webkit-file-upload-button:after {
+    border: 1px solid #cbcbcb;
+    border-bottom: 1px solid #a5a5a5;
+    content: '';
+    position: absolute;
+    left: -1px;
+    height: 25px;
+    width: 80px;
+    bottom: -1px;
+
+    -webkit-border-radius: 4px; 
+    -moz-border-radius: 4px;
+    border-radius: 4px;
+}
+::-webkit-file-upload-button:before { 
+    height: 23px;
+    bottom: -4px;
+    border-top: 0;
+
+    -webkit-border-radius: 0 0 4px 4px; 
+    -moz-border-radius: 0 0 4px 4px;
+    border-radius: 0 0 4px 4px;
+
+
+    -webkit-box-shadow: 0 1px 1px 0px #bfbfbf;
+    -moz-box-shadow: 0 1px 1px 0px #bfbfbf;
+    box-shadow: 0 1px 1px 0px #bfbfbf;
+}
+
+.my_fileInput{
+    width: 100px;
+	height: 100px;
+	
+    color: #0d8ba1;
+    font-weight:bold !important;
+}
+</style>
+""")
+
+# ::-webkit-file-upload-button {
+#   background: #61b93d;
+#   color: white;
+#   padding: 1em;
+#   border-radius: 6px;
+# }
+
+
+
 time = [0]
 current = [0]
 def upload_csv_to_server(attr, old, new):
@@ -52,6 +137,7 @@ s2 = ColumnDataSource(data=dict(x=[], y=[]))
 s3 = ColumnDataSource(data=dict(x=[], y=[]))
 s4 = ColumnDataSource(data=dict(x=time, y=current))
 s5 = ColumnDataSource(data=dict(x=time, y=current))
+s6 = ColumnDataSource(data=dict(x=[0,0,0], y=[0,0,0]))
 
 # Create a plot and style its properties (currently not shown)
 p = figure(plot_height=300, plot_width=600, tools="xpan,pan,reset,box_zoom,save", toolbar_location="above",
@@ -100,7 +186,7 @@ range_tool.x_range.js_on_change('end', callback1)
 
 
 # Plotting data in range (bottom right)
-p2 = figure( plot_height=300,plot_width=600,tools="", title="Watch Here",background_fill_color="#efefef")
+p2 = figure( plot_height=300,plot_width=600,tools="", toolbar_location="above",title="Watch Here",background_fill_color="#efefef")
 p2.line('x', 'y', source=s2, alpha=0.6)
 p2.line('x', 'y', source=s3, alpha=0.6)
 
@@ -185,15 +271,16 @@ def select_functions():
     return selected
 
 # Plot data and the fitting function in the top-right plot and divided data in the bottom-left plot
-p3 = figure( plot_height=300,plot_width=600,tools="xpan,pan,reset,box_zoom,save", title="Origin&Fit",background_fill_color="#efefef")
+p3 = figure( plot_height=300,plot_width=600,tools="xpan,pan,reset,box_zoom,save", toolbar_location="above",title="Origin&Fit",background_fill_color="#efefef")
 p3.line('x', 'y', source=s5, alpha=0.6)
 p3.line('x', 'y', source=s1, alpha=0.6)
 
-p4 = figure( plot_height=300,plot_width=600,tools="",toolbar_location="above", title="Divided",background_fill_color="#efefef")
+p4 = figure( plot_height=300,plot_width=600,tools="xpan,pan,reset,box_zoom,save",toolbar_location="above", title="Divided",background_fill_color="#efefef")
 p4.line('x', 'y', source=s4, alpha=0.6)
 
+
 def update():
-    global s3,s4,s5
+    global s3,s4,s5,s6
     function = select_functions()
     #print(function(1,1,1,1))
     x3 = s3.data['x']
@@ -216,7 +303,7 @@ def update():
         xmin,ymin = min(x3), min(y3)
         x30 = [x - xmin for x in x3]
         y30 = [(y - ymin)*100000 for y in y3]
-        popt, pcov = curve_fit(function, x30, y30,maxfev=50000)
+        popt, pcov = curve_fit(function, x30, y30,maxfev=100000)
         for i in range(len(y3)):
             y3[i] = function(x3[i]-xmin,*popt)/100000 + ymin
         for j in range(len(x4)):
@@ -248,12 +335,16 @@ def update():
         for j in range(len(x4)):
             y5[j] = corrYFactor+function(x4[j]-x3[0],*popt)
             y4[j] = y4[j]/(corrYFactor+function(x4[j]-x3[0],*popt))
-
-
-
+    if invert_selector.value == "Inverting":
+        y4 = [1-y for y in y4]
+    elif invert_selector.value == 'Non-inverting':
+        y4 = [y-1 for y in y4]
+    abc = copy.deepcopy(popt)
     s3.data['y'] = y3
     s4.data['y'] = y4
     s5.data['y'] = y5
+    s6.data['y'] = abc
+    print(abc)
 
 controls = [fittingFunction,invert_selector]
 for control in controls:
@@ -262,5 +353,42 @@ for control in controls:
 range_tool.x_range.on_change('start',lambda attr, old, new: update())
 range_tool.x_range.on_change('end',lambda attr, old, new: update())
 
+# Saving fitting parameters
+saveFuncButton = Button(label="Save Fitting Function Parameters", button_type="success")
+# def paramCall(attr, old, new):
+#     fieldnames = ["Function Type", "a", "b", "c", "x start", "x end"]
+#     pass
+paramCall = CustomJS(args=dict(s2=s2,fittingFunction=fittingFunction,s6=s6),code="""
+        function parameter_to_csv(s2,fittingFunction,abc) {
+            lines = []
+            console.log(abc)
+            row0 = ["Function Type", "a", "b", "c", "x start", "x end"]
+            row1 = [fittingFunction.value, abc[0],abc[1],abc[2], s2.data['x'][0],s2.data['x'][s2.data['x'].length-1]]
+            lines.push(row0.join(','))
+            lines.push(row1.join(','))
+            return lines.join('\\n').concat('\\n')
+        }
+        const filename = 'Parameters.csv'
+        var filetext = parameter_to_csv(s2,fittingFunction,s6.data['y'])
+        const blob = new Blob([filetext], { type: 'text/csv;charset=utf-8;' })
+
+        //addresses IE
+        if (navigator.msSaveBlob) {
+            navigator.msSaveBlob(blob, filename)
+        } else {
+            const link = document.createElement('a')
+            link.href = URL.createObjectURL(blob)
+            link.download = filename
+            link.target = '_blank'
+            link.style.visibility = 'hidden'
+            link.dispatchEvent(new MouseEvent('click'))
+        }
+                    """)
+saveFuncButton.js_on_click(paramCall)
+
+
+
+
+
 # put the button and plot in a layout and add to the document
-curdoc().add_root(column(row(column(file_input_label,file_input),invert_selector,fittingFunction),row(p_select,p3),row(p4,p2),savebutton))
+curdoc().add_root(column(row(column(file_input_label,column(file_input,style)),invert_selector,fittingFunction),row(p_select,p3),row(p4,p2),savebutton,saveFuncButton))
